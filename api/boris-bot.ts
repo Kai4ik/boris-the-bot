0477614012ef968ca6098344db07ee08e5db01a1
Kai <orozobekov.kai@gmail.com>
2024-06-23 18:19:43 -0400

    refactor: business messages mode

--- api/boris-bot.ts
+++ api/boris-bot.ts
@@ -5,15 +5,16 @@ const telegramBotToken = process.env.BOT_TOKEN;
 
 if (!telegramBotToken)
   throw new Error(
-    "Telegram Bot Token needs to be set, otherwise app wont' run"
+    "Telegram Bot Token needs to be set, otherwise app won't run"
   );
 
 const bot = new Bot(telegramBotToken);
 
 const handleMessage = async (ctx: Context) => {
-  console.log(ctx.message);
+  console.log(ctx);
 };
 
 bot.on("message", handleMessage);
+bot.on("business_message", handleMessage);
 
 export default webhookCallback(bot, "https");
